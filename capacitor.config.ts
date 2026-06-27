3027abead57a97e3a31f9625f4ab06ead0bee399
Neelam.Akshit VinaY <akshitvinay4636@gmail.com>
2026-06-27 18:31:41 +0530

    Switch back to assembleDebug to fix App Not Installed (invalid package) error due to unsigned release builds

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,19 +6,9 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     androidScheme: 'https',
-    url: 'https://virahitvin8.github.io/maharaja-bharat-odyssey/',
-    cleartext: true
   },
   android: {
-    buildOptions: {
-      keystorePath: undefined,
-      keystorePassword: undefined,
-      keystoreAlias: undefined,
-      keystoreAliasPassword: undefined,
-    },
     allowMixedContent: true,
-    captureInput: true,
-    webContentsDebuggingEnabled: true,
   },
   plugins: {
     Keyboard: {
